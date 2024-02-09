251e6221f626e9f69b76c5d2717cf708c8875e4e
roy reznik <royreznik@gmail.com>
2024-02-09 12:00:26 +0200

    Adding input file option (#6)
    * Adding input file option
    
    * Fix typing that break typer
    
    * fix make file

--- rexi/cli.py
+++ rexi/cli.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from typing import Annotated, Optional
 
 import typer
 
@@ -10,12 +11,23 @@ app = typer.Typer()
 
 # noinspection SpellCheckingInspection
 @app.command("rexi")
-def rexi_cli() -> None:
-    stdin = sys.stdin.read()
-    try:
-        os.close(sys.stdin.fileno())
-    except OSError:
-        pass
-    sys.stdin = open("/dev/tty", "rb")  # type: ignore[assignment]
-    app: RexiApp[int] = RexiApp(stdin)
+def rexi_cli(
+    input_file: Annotated[
+        Optional[typer.FileText],
+        typer.Option(
+            "--input", "-i",
+            help="Input file to pass to rexi; if not provided, stdin will be used.",
+        ),
+    ] = None,
+) -> None:
+    if input_file:
+        input_text = input_file.read()
+    else:
+        input_text = sys.stdin.read()
+        try:
+            os.close(sys.stdin.fileno())
+        except OSError:
+            pass
+        sys.stdin = open("/dev/tty", "rb")  # type: ignore[assignment]
+    app: RexiApp[int] = RexiApp(input_text)
     app.run()
